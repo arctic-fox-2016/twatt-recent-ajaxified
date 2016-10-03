2903dd46c8269bdb845c14804983157c659e947b
Liliwibi <lili.wibiesono@gmail.com>
2016-10-03 16:19:09 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    accessToken: '39072092-JdxcUUUlf6i5mfUqZz86UFSzFt6EZ5z7raFybmOft',
-    accessTokenSecret: 'AGuunbBvg9WH43Zx4tHRjCHf7sLUAV18lWEfGDNJh7QN9',
-    consumerKey: 'mu7wb3Emw1ZfeodglDim8fmYU',
-    consumerSecret: 'SsKpIVBtAZfJrSfsGFdZtPaxPMY7L11ZvZZnWc9QiiAzusK9KC'
-}
