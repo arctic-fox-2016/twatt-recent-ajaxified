aaaa62d9c6f992411ce8b09f43c92f17385edec3
Liliwibi <email": null>
2016-10-03 16:14:21 +0700

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    accessToken: '	39072092-MfHYYBP39ozZJ1Gxy2U2gb74SJ8PR8wbtCnyiz6aH',
-    accessTokenSecret: 'K3eMtWWnpohwyqE9CueHdc73gzAP46VwAKwsei4PDCuxX',
-    consumerKey: 'M9gL64oF5lqbCD6LTozwUdIpp',
-    consumerSecret: '	rmtj4H6RSnaVZuxmOxdfE0HzvbKij7QPni78qRo5uyQMnCrxcW'
+    accessToken: '39072092-JdxcUUUlf6i5mfUqZz86UFSzFt6EZ5z7raFybmOft',
+    accessTokenSecret: 'AGuunbBvg9WH43Zx4tHRjCHf7sLUAV18lWEfGDNJh7QN9',
+    consumerKey: 'mu7wb3Emw1ZfeodglDim8fmYU',
+    consumerSecret: 'SsKpIVBtAZfJrSfsGFdZtPaxPMY7L11ZvZZnWc9QiiAzusK9KC'
 }
